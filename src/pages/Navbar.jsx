d9f9c45dbdffb9f3321c95192d0271addf721b43
Sumit-Saurabh98 <sumitsaurabh112@gmail.com>
2023-09-09 11:09:11 +0530

    done

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react';
-import { BiMoviePlay} from "react-icons/bi";
+import { RiHotelLine} from "react-icons/ri";
 import {MdAccountCircle} from "react-icons/md";
 import {useNavigate} from "react-router-dom"
 
@@ -26,7 +26,7 @@ function Navbar(props) {
             <div style={{backgroundColor:"#374151"}} className="fixed bottom-0 left-0 z-50 w-full h-16 border-t border-gray-200 dark:bg-gray-700 dark:border-gray-600">
                 <div className="grid h-full max-w-lg grid-cols-4 mx-auto font-medium">
                     <button onClick={()=>navigate("/")} style={{color:"#9BA3AF"}} type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
-                        <span style={{fontSize:"30px"}}><BiMoviePlay/></span>
+                        <span style={{fontSize:"30px"}}><RiHotelLine/></span>
                         <span className="text-sm text-gray-500 dark:text-gray-400 group-hover:text-blue-600 dark:group-hover:text-blue-500">Hotel</span>
                     </button>
                     <button onClick={()=>navigate("/authentication")} style={{color:"#9BA3AF"}} type="button" className="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50 dark:hover:bg-gray-800 group">
